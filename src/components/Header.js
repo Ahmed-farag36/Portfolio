74e6f9e16b64c39cc72c11954688042ea30fadcb
ahmedfarag36 <faragdeveloper@gmail.com>
2019-03-02 21:37:49 +0200

    Testing integration with codesandbox

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = props => (
       src="https://avatars0.githubusercontent.com/u/28810062?v=4"
       alt="my face"
     />
-    <h1 className="name-tag">It's me Ahmed Farag</h1>
+    <h1 className="name-tag">It's me Ahmed Farag HELLO WORLD!!</h1>
     <h2 className="title-tag">MERN Stack Web Developer</h2>
   </div>
 );
